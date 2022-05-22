1d9f672a54261fbdc4bdc467eda1dc4787be402a
denis <denurex@mail.ru>
2022-05-23 01:31:47 +0300

    add urls

--- lr3-4/website/main/urls.py
+++ lr3-4/website/main/urls.py
@@ -9,8 +9,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='main_page'),
-
+    #path('info', views.about, name='info_page'),
+    path('create', views.create, name='create_page'),
+    path('product', views.shop, name='products_page'),
+    path('shopping_cart', views.shop_cart, name='shopcart_page'),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
