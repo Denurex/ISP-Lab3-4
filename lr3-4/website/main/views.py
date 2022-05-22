b52f856733d792c4c023601aae4133e472a8008f
denis <denurex@mail.ru>
2022-05-23 01:31:18 +0300

    Changed logic application

--- lr3-4/website/main/views.py
+++ lr3-4/website/main/views.py
@@ -3,16 +3,39 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
-
+from .models import Comment, Product
+from .forms import CommentForm, UserRegistrationn
 
 
 def index(request):
-
-    return render(request, 'main/index.html', {'title': 'Главная страница'})
-
-
-def about(request):
-    return render(request, 'main/info.html')
+    tasks = Comment.objects.all()
+    return render(request, 'main/base.html')
 
 
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main_page')
+        else:
+            error = 'GG'
+
+
+def check_product(request):
+    product = Product.objects.all()
+    return render(request, 'main/product.html')
+
+
+def shop(request):
+    product = Product.objects.all()
+    context = {
+        'pr': product
+    }
+    return render(request, 'main/product.html', context)
+
+
+def shop_cart(request):
+
+    return render(request, 'main/shopping_cart.html')
